3098300fe4e5fa3cd7d0900b1ae053a8ab18b338
Marcus Bauer <mabako@gmail.com>
2020-08-18 11:07:46 +0200

    Use current branch instead of 'master' as default to reset to

--- vcs/git.go
+++ vcs/git.go
@@ -10,7 +10,30 @@ import (
 	"strings"
 )
 
-const defaultRef = "master"
+func getRef(dir string) (string, error) {
+	cmd := exec.Command(
+		"git",
+		"branch",
+		"--show-current")
+	cmd.Dir = dir
+	r, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	defer r.Close()
+
+	if err := cmd.Start(); err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+
+	if _, err := io.Copy(&buf, r); err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(buf.String()), cmd.Wait()
+}
 
 func init() {
 	Register(newGit, "git")
@@ -50,6 +73,8 @@ func (g *GitDriver) HeadRev(dir string) (string, error) {
 func run(desc, dir, cmd string, args ...string) error {
 	c := exec.Command(cmd, args...)
 	c.Dir = dir
+
+	//log.Printf("Executing %s", c)
 	if out, err := c.CombinedOutput(); err != nil {
 		log.Printf(
 			"Failed to %s %s, see output below\n%sContinuing...",
@@ -62,6 +87,16 @@ func run(desc, dir, cmd string, args ...string) error {
 }
 
 func (g *GitDriver) Pull(dir string) (string, error) {
+	//log.Printf("Pull %s", dir)
+
+	branch, err := getRef(dir)
+	if err != nil {
+		log.Printf(
+			"Failed to retrieve branch for %s",
+			dir)
+		return "", err
+	}
+
 	if err := run("git fetch", dir,
 		"git",
 		"fetch",
@@ -69,7 +104,7 @@ func (g *GitDriver) Pull(dir string) (string, error) {
 		"--no-tags",
 		"--depth", "1",
 		"origin",
-		fmt.Sprintf("+%s:remotes/origin/%s", defaultRef, defaultRef)); err != nil {
+		fmt.Sprintf("+%s:remotes/origin/%s", branch, branch)); err != nil {
 		return "", err
 	}
 
@@ -77,7 +112,7 @@ func (g *GitDriver) Pull(dir string) (string, error) {
 		"git",
 		"reset",
 		"--hard",
-		fmt.Sprintf("origin/%s", defaultRef)); err != nil {
+		fmt.Sprintf("origin/%s", branch)); err != nil {
 		return "", err
 	}
 
